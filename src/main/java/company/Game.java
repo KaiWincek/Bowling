495faf6df82a7f2cc57ab3c7a5ac733fb81f7ac8
KaiWincek <kai.wincek@gmail.com>
2021-05-10 14:18:09 +0200

    Formatting

--- src/main/java/company/Game.java
+++ src/main/java/company/Game.java
@@ -5,44 +5,46 @@ import java.util.List;
 
 public class Game {
 
-    public int calculateScore(String scorecard){
+    public int calculateScore(String scorecard) {
         int score = 0;
         List<Roll> rollList = fillRollList(scorecard);
 
-        for (int i = 0; i < rollList.size(); i++){
+        for (int i = 0; i < rollList.size(); i++) {
             Roll currentRoll = rollList.get(i);
-            switch (currentRoll.getRollType()){
+            switch (currentRoll.getRollType()) {
                 case SPARE:
-                    score += currentRoll.getValue() + rollList.get(i+1).getValue();
+                    score += currentRoll.getValue() + rollList.get(i + 1).getValue();
                     break;
                 case STRIKE:
-                    score += currentRoll.getValue() + rollList.get(i+1).getValue() + rollList.get(i+2).getValue();
+                    score += currentRoll.getValue() + rollList.get(i + 1).getValue() + rollList.get(i + 2).getValue();
                     break;
                 default:
                     score += currentRoll.getValue();
             }
+            
+            //Checking for Bonus Rolls
             if (
-                    (currentRoll.getRollType() == RollType.STRIKE && i >= rollList.size()-3) ||
-                    (currentRoll.getRollType() == RollType.SPARE && i >= rollList.size()-2)
-            ){
+                    (currentRoll.getRollType() == RollType.STRIKE && i >= rollList.size() - 3) ||
+                    (currentRoll.getRollType() == RollType.SPARE && i >= rollList.size() - 2)
+            ) {
                 return score;
             }
         }
         return score;
     }
 
-    private List<Roll> fillRollList(String scorecard){
+    private List<Roll> fillRollList(String scorecard) {
         List<Roll> rollList = new ArrayList<>();
-        for (int i = 0; i < scorecard.length(); i++){
+        for (int i = 0; i < scorecard.length(); i++) {
             char c = scorecard.charAt(i);
-            switch (c){
-                case '/' :
-                    rollList.add(new Roll(10 - rollList.get(i-1).getValue(), RollType.SPARE));
+            switch (c) {
+                case '/':
+                    rollList.add(new Roll(10 - rollList.get(i - 1).getValue(), RollType.SPARE));
                     break;
-                case 'x' :
+                case 'x':
                     rollList.add(new Roll(10, RollType.STRIKE));
                     break;
-                case '-' :
+                case '-':
                     rollList.add(new Roll(0, RollType.VALUE));
                     break;
                 default:
